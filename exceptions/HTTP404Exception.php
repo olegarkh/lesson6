e2c6517892b032702c93282c8aeed1e985ac0241
Oleg <oarch@mail.ru>
2015-03-09 19:55:46 +0300

    Класс исключений запросов

--- exceptions/HTTP404Exception.php
+++ exceptions/HTTP404Exception.php
@@ -1,16 +1,5 @@
 <?php
 
-/*class ModelException
-        extends Exception
-{
-
-}*/
-
-/*class HTTP403Exception
-         extends PDOException
-{
-    public $code_answer = ' Исключение HTTP403 ';
-}*/
 
 class HTTP404Exception
          extends Exception
