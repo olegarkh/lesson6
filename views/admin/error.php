24752b8d64ded9d56e04c0722abcf08f348ccfe3
Oleg <oarch@mail.ru>
2015-03-09 19:48:22 +0300

    Форма сообщений об исключениях

---
+++ views/admin/error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ошибка</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h2><?php echo $error->code_answer; ?></h2>
+    <h2><?php //echo $error; ?></h2>
+    <h2><?php echo 0 != $error->getCode(); ?></h2>
+    <p><?php echo $error->getMessage(); ?></p>
+    <p><?php echo $error->errorInfo; ?></p>
+    <p><?php //var_dump($error);?></p>
+    <a href="index.php?ctrl=News&act=All">Вернуться на сайт</a>
+</body>
