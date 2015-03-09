50e8f5a48fb0a9bb986743368b4c231f4fbb7db1
Oleg <oarch@mail.ru>
2015-03-09 19:45:15 +0300

    Модель новостей

---
+++ models/NewsModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class NewsModel
+ * @property $id
+ * @property $title
+ * @property $text
+ */
+
+class NewsModel
+      extends AbstractModel
+{
+     protected static $table = 'news';
+}
